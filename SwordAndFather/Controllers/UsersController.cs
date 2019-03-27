aa71175bb5ee3bb50f7d095a9cb233f8664ba06b
Nathan Gonzalez <ncgonz@gmail.com>
2019-03-26 21:40:43 -0500

    creating our first model and controller for registering a user.

---
+++ SwordAndFather/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwordAndFather.Models;
+
+namespace SwordAndFather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        static List<User> _users = new List<User>();
+
+        [HttpPost("register")]
+        public ActionResult<int> AddUser(CreateUserRequest createRequest)
+        {
+            //validation
+            if (string.IsNullOrEmpty(createRequest.Username)
+                && string.IsNullOrEmpty(createRequest.Password))
+            {
+                return BadRequest(new { error = "users must have a username and password" });
+            }
+
+            var newUser = new User(createRequest.Username, createRequest.Password);
+
+            newUser.Id = _users.Count + 1;
+
+            _users.Add(newUser);
+
+            return Created($"api/users/{newUser.Id}", newUser);
+        }
+
+    }
+}
---
+++ SwordAndFather/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwordAndFather.Models
+{
+    public class User
+    {
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
